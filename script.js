7384646e69469b801c9a3efef103b5e1d22f43c1
Ethan Johnston <ethan_johnston13@hotmail.com>
2023-01-15 19:05:25 -0800

    add /chatbot to render.com link

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ $(document).ready(function() {
 		const message = $('#chatbot-input').val();
 		$.ajax({
 			type: 'POST',
-			url: 'https://comp-4537-lab-2-8ew2.onrender.com',
+			url: 'https://comp-4537-lab-2-8ew2.onrender.com/chatbot',
             // url: 'http://localhost:3000/chatbot',
 			data: {
 				message: message
